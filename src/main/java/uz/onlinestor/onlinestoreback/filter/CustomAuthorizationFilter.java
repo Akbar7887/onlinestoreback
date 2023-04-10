ca7775df0e134ade3bbbbf2c4fd01da16221bddc
Akbar <akbar358888@gmail.com>
2023-04-10 13:47:15 +0500

    i

--- src/main/java/uz/onlinestor/onlinestoreback/filter/CustomAuthorizationFilter.java
+++ src/main/java/uz/onlinestor/onlinestoreback/filter/CustomAuthorizationFilter.java
@@ -10,11 +10,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -30,9 +25,9 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(javax.servlet.http.HttpServletRequest request,
+                                    javax.servlet.http.HttpServletResponse response,
+                                    javax.servlet.FilterChain filterChain) throws javax.servlet.ServletException, IOException{
 
         if (request.getServletPath().equals("/login")
                 || request.getServletPath().equals("/api/token/refresh")
@@ -75,4 +70,17 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
             }
         }
     }
+
+    /**
+     * Same contract as for {@code doFilter}, but guaranteed to be
+     * just invoked once per request within a single request thread.
+     * See {@link #shouldNotFilterAsyncDispatch()} for details.
+     * <p>Provides HttpServletRequest and HttpServletResponse arguments instead of the
+     * default ServletRequest and ServletResponse ones.
+     *
+     * @param request
+     * @param response
+     * @param filterChain
+     */
+
 }
